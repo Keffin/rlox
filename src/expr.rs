d6666fb9a671486fa2fc40a3dfde668b4b0e0f5a
Keffin <kevin.alemi97@gmail.com>
2022-08-05 19:44:36 +0200

    feat: add different expr types and tmp solution

--- src/expr.rs
+++ src/expr.rs
@@ -2,8 +2,26 @@ use crate::token::Token;
 
 pub enum Expr {
     Binary {
+        // Recursive types, need to Box
         left: Box<Expr>,
         operator: Token,
         right: Box<Expr>,
     },
+    Unary {
+        operator: Token,
+        right: Box<Expr>,
+    },
+    Literal(LiteralRepresentations),
+    Grouping(Box<Expr>),
+    // Temp solution for handling end scenario of primary
+    FailScenario {
+        reason: String,
+    },
+}
+
+pub enum LiteralRepresentations {
+    Boolean(bool),
+    Nil,
+    Number(f64),
+    String(String),
 }
