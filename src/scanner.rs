896cdbddf46b81a8a5eb56a26ef134db762486dd
Keffin <kevin.alemi97@gmail.com>
2022-08-05 14:49:25 +0200

    feat: scanner impl

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 use crate::lox::Lox;
-use crate::token::Token;
+use crate::token::{self, Token};
 use crate::token_type::TokenType;
 
 struct Scanner {
@@ -8,6 +10,7 @@ struct Scanner {
     start: u32,
     current: u32,
     line: u32,
+    keywords: HashMap<String, TokenType>,
 }
 
 impl Scanner {
@@ -18,11 +21,29 @@ impl Scanner {
             start: 0,
             current: 0,
             line: 1,
+            keywords: HashMap::from([
+                (String::from("and"), TokenType::AND),
+                (String::from("class"), TokenType::CLASS),
+                (String::from("else"), TokenType::ELSE),
+                (String::from("false"), TokenType::FALSE),
+                (String::from("for"), TokenType::FOR),
+                (String::from("fun"), TokenType::FUN),
+                (String::from("if"), TokenType::IF),
+                (String::from("nil"), TokenType::NIL),
+                (String::from("or"), TokenType::OR),
+                (String::from("print"), TokenType::PRINT),
+                (String::from("return"), TokenType::RETURN),
+                (String::from("super"), TokenType::SUPER),
+                (String::from("this"), TokenType::THIS),
+                (String::from("true"), TokenType::TRUE),
+                (String::from("var"), TokenType::VAR),
+                (String::from("while"), TokenType::WHILE),
+            ]),
         }
     }
 
     pub fn scan_tokens(&mut self) -> &Vec<Token> {
-        while (!self.is_at_end()) {
+        while !self.is_at_end() {
             self.start = self.current;
             //scan_token();
         }
@@ -34,6 +55,7 @@ impl Scanner {
             self.line,
         ));
 
+        // Return reference or add clone to Token type struct.
         return &self.tokens;
     }
 
@@ -54,7 +76,60 @@ impl Scanner {
             '+' => self.add_token(TokenType::PLUS, "Null".to_string()),
             ';' => self.add_token(TokenType::SEMICOLON, "Null".to_string()),
             '*' => self.add_token(TokenType::STAR, "Null".to_string()),
-            _ => lox.error(self.line, "Unexpected character"),
+            '!' => {
+                if self.matches('=') {
+                    self.add_token(TokenType::BANGEQUAL, "Null".to_string())
+                } else {
+                    self.add_token(TokenType::BANG, "Null".to_string())
+                }
+            }
+            '=' => {
+                if self.matches('=') {
+                    self.add_token(TokenType::EQUALEQUAL, "Null".to_string())
+                } else {
+                    self.add_token(TokenType::EQUAL, "Null".to_string())
+                }
+            }
+            '<' => {
+                if self.matches('=') {
+                    self.add_token(TokenType::LESSEQUAL, "Null".to_string())
+                } else {
+                    self.add_token(TokenType::LESS, "Null".to_string())
+                }
+            }
+            '>' => {
+                if self.matches('=') {
+                    self.add_token(TokenType::GREATEREQUAL, "Null".to_string())
+                } else {
+                    self.add_token(TokenType::GREATER, "Null".to_string())
+                }
+            }
+            '/' => {
+                if self.matches('/') {
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::SLASH, "Null".to_string());
+                }
+            }
+            ' ' | '\r' | '\t' => (),
+            '\n' => self.line += 1,
+            '"' => self.string(lox),
+            'o' => {
+                if self.matches('r') {
+                    self.add_token(TokenType::OR, "Null".to_string());
+                }
+            }
+            _ => {
+                if self.is_digit(c) {
+                    self.number()
+                } else if self.is_alpha(c) {
+                    self.identifier()
+                } else {
+                    lox.error(self.line, "Unexpected character")
+                }
+            }
         };
     }
 
@@ -68,5 +143,110 @@ impl Scanner {
         let text: &str = &self.source[self.start as usize..self.current as usize];
         self.tokens
             .push(Token::new(token_type, text.to_string(), literal, self.line))
+    }
+
+    fn matches(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+
+        if self.source.chars().nth(self.current as usize).unwrap() != expected {
+            return false;
+        }
+
+        self.current += 1;
+        return true;
+    }
+
+    fn peek(&self) -> char {
+        if self.is_at_end() {
+            return '\n';
+        }
+        return self.source.chars().nth(self.current as usize).unwrap();
+    }
+
+    fn string(&mut self, mut lox: Lox) {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            lox.error(self.line, "Unterminated string");
+        }
+
+        self.advance();
+
+        let start_inc: u32 = self.start + 1;
+        let current_dec: u32 = self.current - 1;
+
+        let value: &str = &self.source[start_inc as usize..current_dec as usize];
+        self.add_token(TokenType::STRING, value.to_string())
+    }
+
+    fn is_digit(&self, c: char) -> bool {
+        return c >= '0' && c <= '9';
+    }
+
+    fn number(&mut self) {
+        while self.is_digit(self.peek()) {
+            self.advance();
+        }
+
+        if self.peek() == '.' && self.is_digit(self.peek_next()) {
+            self.advance();
+
+            while self.is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        let num_to_str: &str = &self.source[self.start as usize..self.current as usize];
+        self.add_token(TokenType::NUMBER, num_to_str.to_string())
+    }
+
+    fn peek_next(&self) -> char {
+        if self.current + 1 >= self.source.len() as u32 {
+            return '\0';
+        }
+        return self.source.chars().nth(self.current as usize + 1).unwrap();
+    }
+
+    fn identifier(&mut self) {
+        while self.is_alpha_numeric(self.peek()) {
+            self.advance();
+        }
+
+        let text: &str = &self.source[self.start as usize..self.current as usize];
+        let token_type: Option<&TokenType> = self.keywords.get(&text.to_string());
+        match token_type {
+            Some(TokenType::AND) => self.add_token(TokenType::AND, "Null".to_string()),
+            Some(TokenType::CLASS) => self.add_token(TokenType::CLASS, "Null".to_string()),
+            Some(TokenType::ELSE) => self.add_token(TokenType::ELSE, "Null".to_string()),
+            Some(TokenType::FALSE) => self.add_token(TokenType::FALSE, "Null".to_string()),
+            Some(TokenType::FOR) => self.add_token(TokenType::FOR, "Null".to_string()),
+            Some(TokenType::FUN) => self.add_token(TokenType::FUN, "Null".to_string()),
+            Some(TokenType::IF) => self.add_token(TokenType::IF, "Null".to_string()),
+            Some(TokenType::NIL) => self.add_token(TokenType::NIL, "Null".to_string()),
+            Some(TokenType::OR) => self.add_token(TokenType::OR, "Null".to_string()),
+            Some(TokenType::PRINT) => self.add_token(TokenType::PRINT, "Null".to_string()),
+            Some(TokenType::RETURN) => self.add_token(TokenType::RETURN, "Null".to_string()),
+            Some(TokenType::SUPER) => self.add_token(TokenType::SUPER, "Null".to_string()),
+            Some(TokenType::THIS) => self.add_token(TokenType::THIS, "Null".to_string()),
+            Some(TokenType::TRUE) => self.add_token(TokenType::TRUE, "Null".to_string()),
+            Some(TokenType::VAR) => self.add_token(TokenType::VAR, "Null".to_string()),
+            Some(TokenType::WHILE) => self.add_token(TokenType::WHILE, "Null".to_string()),
+            _ => self.add_token(TokenType::IDENTIFIER, "Null".to_string()),
+        };
+    }
+
+    fn is_alpha_numeric(&self, c: char) -> bool {
+        return self.is_alpha(c) || self.is_digit(c);
+    }
+
+    fn is_alpha(&self, c: char) -> bool {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
     }
 }
